c5f9157a1808783f2570da9032a15df12d90aa1b
wowiwj <35649084@qq.com>
2015-10-07 21:15:10 +0800

    让主角跑动

--- RunRunRun/Classes/Player.cpp
+++ RunRunRun/Classes/Player.cpp
@@ -13,14 +13,31 @@ bool Player::init()
 {
     
     
-    
-    
     return true;
 }
 
 void Player::run()
 {
     
+    SpriteFrameCache* frameCache = SpriteFrameCache::getInstance();
+    frameCache->addSpriteFramesWithFile("boys.plist", "boys.png");
     
+    SpriteFrame* frame = NULL;
     
+    //创建精灵帧对象，添加到列表
+    Vector<SpriteFrame*> framelist;
+    for (int i = 1 ; i < 15; i ++) {
+        frame = frameCache->getSpriteFrameByName(StringUtils::format("run%d.png",i));
+        
+        framelist.pushBack(frame);
+    }
+    
+    //根据精灵帧对象创建动画
+    Animation* animation = Animation::createWithSpriteFrames(framelist);
+    animation->setLoops(-1);//循环播放
+    animation->setDelayPerUnit(0.08f);//每帧播放的间隔
+    
+    Animate* animate = Animate::create(animation);
+    
+    m_sprite->runAction(animate);
 }
